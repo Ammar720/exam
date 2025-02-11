d7f985e2abc7333252d0a0a93da3ef49772f8309
Ammar Ahmed <ammare.ba720.com>
2025-02-11 12:13:16 +0200

    Iinitlize project

---
+++ lib/core/resources/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color blue = Color(0xff02369C);
+  static const Color white = Color(0xffF9F9F9);
+  static const Color black = Color(0xff0F0F0F);
+  static const Color gray = Color(0xff535353);
+  static const Color error = Color(0xffCC1010);
+  static const Color sucess = Color(0xff11CE19);
+  static const Color lightBlue = Color(0xffEDEFF3);
+  static const Color lightRed = Color(0xffF8D2D2);
+  static const Color placeHolder = Color(0xffA6A6A6);
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:exam/features/auth/presentation/screens/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const Exam());
+}
+
+class Exam extends StatelessWidget {
+  const Exam({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, __) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+         routes: {
+          Login.routeName : (context) => const Login(),
+         },
+         initialRoute: Login.routeName,
+        ),
+      );
+  }
+}
